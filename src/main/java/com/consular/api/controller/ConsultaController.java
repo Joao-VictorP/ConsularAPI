cb52e84fab79f0338698468783470c9ae1fdb93b
unknown <jvictor0125@hotmail.com>
2020-06-29 23:16:26 -0300

    Gets do Consultório

--- src/main/java/com/consular/api/controller/ConsultaController.java
+++ src/main/java/com/consular/api/controller/ConsultaController.java
@@ -1,9 +1,16 @@
 package com.consular.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.consular.domain.model.Consulta;
+import com.consular.domain.model.Consultorio;
 import com.consular.domain.repository.ConsultaRepository;
 
 @RestController
@@ -12,4 +19,20 @@ public class ConsultaController {
 
 	@Autowired
 	private ConsultaRepository consultaRepository;
+
+	@GetMapping
+	public List<Consulta> listar() {
+		return consultaRepository.findAll();
+	}
+
+	@GetMapping("/{codConsultorio}")
+	public ResponseEntity<List<Consulta>> buscarCodConsultorio(@PathVariable Consultorio codConsultorio) {
+		List<Consulta> consulta = consultaRepository.findByConsultorio(codConsultorio);
+
+		if (!consulta.isEmpty()) {
+			return ResponseEntity.ok(consulta);
+		}
+
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/consular/domain/repository/ConsultaRepository.java
+++ src/main/java/com/consular/domain/repository/ConsultaRepository.java
@@ -1,11 +1,15 @@
 package com.consular.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.consular.domain.model.Consulta;
+import com.consular.domain.model.Consultorio;
 
 @Repository
 public interface ConsultaRepository extends JpaRepository<Consulta, Long>{
 
+	List<Consulta> findByConsultorio(Consultorio codConsultorio);
 }
