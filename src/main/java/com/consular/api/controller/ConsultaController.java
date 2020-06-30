50e9a65ffcb81f3c58513b0aadbb1abc8afcdfb6
unknown <jvictor0125@hotmail.com>
2020-06-29 23:29:44 -0300

    Todos os endpoints

--- src/main/java/com/consular/api/controller/ConsultaController.java
+++ src/main/java/com/consular/api/controller/ConsultaController.java
@@ -2,11 +2,19 @@ package com.consular.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.consular.domain.model.Consulta;
@@ -33,6 +41,35 @@ public class ConsultaController {
 			return ResponseEntity.ok(consulta);
 		}
 
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Consulta criarConsulta(@RequestBody @Valid Consulta consulta) {
+		return consultaRepository.save(consulta);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Consulta> atualizarConsulta(@PathVariable Long id, @RequestBody @Valid Consulta consulta){
+		if(!consultaRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		consulta.setId(id);
+		consultaRepository.save(consulta);
+
+		return ResponseEntity.ok(consulta);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> removerConsulta(@PathVariable Long id) {
+		if(!consultaRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		consultaRepository.deleteById(id);
+		
 		return ResponseEntity.noContent().build();
 	}
 }
